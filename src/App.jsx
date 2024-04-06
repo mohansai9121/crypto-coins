5ac00a8e1489c2aacb383c5a7abf2eb5b376c4fa
Mohan Sai <mohansaichinthala9121@gmail.com>
2024-04-06 11:58:51 +0530

    installed axios and fetched Crypto-coins api data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import './App.css'
+import axios from 'axios'
 
 const App = () => {
+  axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(
+    (response)=>{
+      console.log(response)
+    }
+  ).catch((err)=>{
+    console.log(err)
+  })
   return (
     <div>
       <div  className="heading">
