28a1b988d24f64792ba73c3e54cb502d8baebbd5
Mohan Sai <mohansaichinthala9121@gmail.com>
2024-04-13 13:48:06 +0530

    Updated displaying time and date

--- src/pages/Coin.jsx
+++ src/pages/Coin.jsx
@@ -21,9 +21,10 @@ const Coin = () => {
                     <img src={coin1.image} alt={coin1.name} className="singleImage"/><br/>
                     <p style={{fontSize:'large'}}><b>Name:</b>{coin1.name}</p>
                     <p style={{fontSize:'large'}}><b>Market Rank:</b>{coin1.market_cap_rank}</p>
-                    <p style={{fontSize:'large'}}><b>Symbol:</b>{coin1.symbol}</p>
+                    <p style={{fontSize:'large'}}><b>Symbol:</b>{" "}{coin1.symbol}</p>
                     <p style={{fontSize:'large'}}><b>Value:</b>${coin1.current_price}</p>
-                    <p style={{fontSize:'large'}}><b>Updated on:</b>{coin1.last_updated}</p>
+                    <p style={{fontSize:'large'}}><b>Updated on:</b>{coin1.last_updated.slice(0,10)}</p>
+                    <p style={{fontSize:'large'}}><b>Updated time:</b>{coin1.last_updated.slice(11,23)}</p>
                     <p style={{fontSize:'large'}}><b>Max Supply:</b>{" "}{coin1.max_supply?coin1.max_supply:"--"}</p>
                     <p style={{fontSize:'large'}}><b>Total Supply:</b>{coin1.total_supply}</p>
                     <p style={{fontSize:'large'}}><b>Price Change percentage:</b>{" "}{coin1.ath_change_percentage}</p>
